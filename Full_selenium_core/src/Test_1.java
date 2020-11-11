cb10fbe768f04c761adfb313f12b0c17fb4a95ed
liesnychevskyi <liesnychevskyi.s@gmail.com>
2020-11-11 10:52:07 +0200

    Added an User ID print statement

--- Full_selenium_core/src/Test_1.java
+++ Full_selenium_core/src/Test_1.java
@@ -4,6 +4,7 @@ public class Test_1
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello from Mac machine <<Windows>>");
+		System.out.println("User ID is: 337771111 <<Windows>>");
 	}
 
 }
